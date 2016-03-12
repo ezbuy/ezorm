0e207884a44ce9215b6d35cf3e057346130f4275
zhaokongsheng <zhao.kongsheng@gmail.com>
2016-03-12 14:39:23 +0800

    Change mssql example data file

--- example/mssql_people/People.sql
+++ example/mssql_people/People.sql
@@ -1,57 +1,26 @@
-/*
-Navicat SQL Server Data Transfer
-
-Source Server         : localtest
-Source Server Version : 110000
-Source Host           : localhost:1433
-Source Database       : test
-Source Schema         : dbo
-
-Target Server Type    : SQL Server
-Target Server Version : 110000
-File Encoding         : 65001
-
-Date: 2016-03-12 12:27:00
-*/
-
-
--- ----------------------------
--- Table structure for People
--- ----------------------------
-DROP TABLE [dbo].[People]
-GO
-CREATE TABLE [dbo].[People] (
-[Age] int NOT NULL ,
-[Name] nvarchar(255) NOT NULL ,
-[PeopleId] int NOT NULL IDENTITY(1,1) ,
-[NonIndexA] nchar(255) NULL ,
-[NonIndexB] nchar(255) NULL ,
-[IndexAPart1] int NULL ,
-[IndexAPart2] int NULL ,
-[IndexAPart3] int NULL 
-)
-
-
-GO
-DBCC CHECKIDENT(N'[dbo].[People]', RESEED, 1590)
+USE [test]
 GO
 
--- ----------------------------
--- Indexes structure for table People
--- ----------------------------
-CREATE INDEX [IX_people_age] ON [dbo].[People]
-([Age] ASC) 
-GO
-CREATE UNIQUE INDEX [IX_people_name] ON [dbo].[People]
-([Name] ASC) 
-WITH (IGNORE_DUP_KEY = ON)
-GO
-CREATE INDEX [IX_People_A] ON [dbo].[People]
-([IndexAPart1] ASC, [IndexAPart2] ASC, [IndexAPart3] ASC) 
+/****** Object:  Table [dbo].[People]    Script Date: 2016/3/12 14:27:41 ******/
+SET ANSI_NULLS ON
 GO
 
--- ----------------------------
--- Primary Key structure for table People
--- ----------------------------
-ALTER TABLE [dbo].[People] ADD PRIMARY KEY ([PeopleId])
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[People](
+	[Age] [int] NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[PeopleId] [int] IDENTITY(1,1) NOT NULL,
+	[NonIndexA] [nchar](255) NULL,
+	[NonIndexB] [nchar](255) NULL,
+	[IndexAPart1] [int] NULL,
+	[IndexAPart2] [int] NULL,
+	[IndexAPart3] [int] NULL,
+ CONSTRAINT [PK__people__3214EC27D30B5FDA] PRIMARY KEY CLUSTERED
+(
+	[PeopleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
 GO
