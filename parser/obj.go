e40ae715d7edc679f252d3e1ba16da78f72b011b
陈冶 <chenye@ezbuy.com>
2016-11-25 18:48:33 +0800

    [mysql] support non auto increase id

--- parser/obj.go
+++ parser/obj.go
@@ -100,7 +100,11 @@ func (o *Obj) init() {
 
 func (o *Obj) GetFieldNameWithDB(name string) string {
 	if o.DbName != "" {
-		return fmt.Sprintf("%s.%s", camel2name(o.DbName), name)
+		dbname := o.DbName
+		if o.Db == "mysql" {
+			dbname = camel2name(o.DbName)
+		}
+		return fmt.Sprintf("%s.%s", dbname, name)
 	}
 	return name
 }
