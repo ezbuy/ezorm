b9ddd618af5dd4a991b9ece80ea820a5a08e79b5
HackerZ <hackerzgz@gmail.com>
2019-11-28 16:10:22 +0800

    test: count and counte unit test passed

--- example/blog/blog_test.go
+++ example/blog/blog_test.go
@@ -34,3 +34,48 @@ func TestBlogSave(t *testing.T) {
 
 	fmt.Printf("get blog ok: %#v", b)
 }
+
+func TestBlogCount(t *testing.T) {
+	now := time.Now() // get current time.Time as `Save` and `Count` condition
+	p := Get_BlogMgr().NewBlog()
+	p.Title = "ezorm counter"
+	p.Slug = fmt.Sprintf("%d", now.Nanosecond())
+
+	if _, err := p.Save(); err != nil {
+		t.Fatalf("failed to save blog: %v", err)
+	}
+
+	result := Get_BlogMgr().Count(db.M{
+		BlogMgoFieldTitle: "ezorm counter",
+		BlogMgoFieldSlug:  fmt.Sprintf("%d", now.Nanosecond()),
+	})
+	if result != 1 {
+		t.Fatalf("return value of Count should equal 1, got: %d", result)
+	}
+
+	Get_BlogMgr().RemoveBySlug(p.Slug) // cleanup the test environment
+}
+
+func TestBlogCountE(t *testing.T) {
+	now := time.Now() // get current time.Time as `Save` and `CountE` condition
+	p := Get_BlogMgr().NewBlog()
+	p.Title = "ezorm counter"
+	p.Slug = fmt.Sprintf("%d", now.Nanosecond())
+
+	if _, err := p.Save(); err != nil {
+		t.Fatalf("failed to save blog: %v", err)
+	}
+
+	result, err := Get_BlogMgr().CountE(db.M{
+		BlogMgoFieldTitle: "ezorm counter",
+		BlogMgoFieldSlug:  fmt.Sprintf("%d", now.Nanosecond()),
+	})
+	if err != nil {
+		t.Fatalf("failed to run CountE: %v", err)
+	}
+	if result != 1 {
+		t.Fatalf("return value of CountE should equal 1, got: %d", result)
+	}
+
+	Get_BlogMgr().RemoveBySlug(p.Slug) // cleanup the test environment
+}
