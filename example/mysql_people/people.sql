567948b86f8f6f6580fe66fbdea83bc825d30139
hackerzgz <hackerzgz@gmail.com>
2020-10-20 19:55:46 +0800

    ci: fix create table script syntax error

--- example/mysql_people/people.sql
+++ example/mysql_people/people.sql
@@ -1,23 +1,23 @@
-CREATE TABLE test.Blog
+CREATE TABLE `test.Blog`
 (
-    blog_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title VARCHAR(1000),
-    hits INT(11),
-    slug VARCHAR(20),
-    body TEXT,
-    user INT(11),
-    is_published TINYINT(1),
+    `blog_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(1000),
+    `hits` INT(11),
+    `slug` VARCHAR(20),
+    `body` TEXT,
+    `user` INT(11),
+    `is_published` TINYINT(1),
     `create` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     `update` DATETIME,
-    column_10 TIMESTAMP,
-    time_stamp BIGINT(20)
+    `column_10` TIMESTAMP,
+    `time_stamp` BIGINT(20)
 );
-CREATE UNIQUE INDEX Blog_Slug_uindex ON test.Blog (slug);
+CREATE UNIQUE INDEX Blog_Slug_uindex ON `test.Blog` (slug);
 
-CREATE TABLE test.test_user
+CREATE TABLE `test.test_user`
 (
-    user_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100),
-    user_number INT(11)
+    `user_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100),
+    `user_number` INT(11)
 );
-CREATE UNIQUE INDEX test_user_user_number_uindex ON test.test_user (user_number);
+CREATE UNIQUE INDEX test_user_user_number_uindex ON `test.test_user` (`user_number`);
