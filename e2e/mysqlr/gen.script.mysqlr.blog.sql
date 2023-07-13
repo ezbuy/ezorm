aaaaeeac802d5c96026715c280695e68537fdc52
Nace Sc <scbizu@gmail.com>
2023-07-13 11:12:55 +0800

    internal/parser/mysqlr: reform CREATE TABLE unique key name (#285)

--- e2e/mysqlr/gen.script.mysqlr.blog.sql
+++ e2e/mysqlr/gen.script.mysqlr.blog.sql
@@ -12,6 +12,6 @@ CREATE TABLE `blogs` (
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` BIGINT(20) NOT NULL DEFAULT '0',
 	PRIMARY KEY(`id`,`user_id`),
-	UNIQUE KEY `uniq_title_of_blog_uk` (`title`),
+	UNIQUE KEY `uk_title` (`title`),
 	KEY `idx_status` (`status`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'blogs';
 
