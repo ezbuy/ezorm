aede536813dbd7371393c88daf743f3664b07ebe
Nace Sc <scbizu@gmail.com>
2023-09-04 03:31:28 -0500

    v2/pkg/orm: IsErrNotFound includes mongo no documents (#309)

--- v2/pkg/orm/errors.go
+++ v2/pkg/orm/errors.go
@@ -3,9 +3,11 @@ package orm
 import (
 	"database/sql"
 	"errors"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // IsErrorNotFound is a sql.ErrNoRows wrapper
 func IsErrNotFound(err error) bool {
-	return errors.Is(err, sql.ErrNoRows)
+	return errors.Is(err, sql.ErrNoRows) || errors.Is(err, mongo.ErrNoDocuments)
 }
